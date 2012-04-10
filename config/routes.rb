41d8a2f223d91eae8f32b7a79e08ec3acfb33073
David30 <DavidH@yandex.ru>
2012-04-10 06:05:19 +0600

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
  root :to => 'pages#home'
-root_path => '/'
-root_url  => 'http://localhost:3000/'
+
 
 	get "pages/about"
 
